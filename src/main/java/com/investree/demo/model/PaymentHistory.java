d23369f2ea697990940e6ec0fc3f8cf8e6e2f3e3
unknown <muhammad_akbar287@outlook.com>
2022-05-29 14:46:05 +0700

    first commit

---
+++ src/main/java/com/investree/demo/model/PaymentHistory.java
@@ -0,0 +1,40 @@
+package com.investree.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "payment_history")
+public class PaymentHistory implements Serializable {
+
+    private static final long serialVersionUID = 5267477974389610043L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_transaksi")
+    private Transaksi transaksi;
+
+    @Column(name = "pembayaran_ke")
+    private int pembayaranKe;
+
+    @Column(name = "jumlah")
+    private Double jumlah;
+
+    @Column(name = "bukti_pembayaran")
+    private String bukti_pembayaran;
+}
---
+++ src/main/java/com/investree/demo/model/Users.java
@@ -0,0 +1,45 @@
+package com.investree.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class Users implements Serializable{
+
+    private static final long serialVersionUID = 5914477974389610248L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+
+    @OneToMany(mappedBy = "peminjam")
+    private Transaksi peminjam;
+
+    @OneToMany(mappedBy = "meminjam")
+    private Transaksi meminjam;
+
+    @OneToOne(mappedBy = "user")
+    private UserDetail userDetail;
+
+    @Column(name = "username")
+    private String username;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "is_active")
+    private Boolean is_active;
+}
