22469e3574e4d5d43d8696cd0c8633161fcf9010
Akbar287 <49776425+Akbar287@users.noreply.github.com>
2022-06-03 09:58:59 +0700

    feature paging

--- src/main/java/com/investree/demo/view/TransaksiService.java
+++ src/main/java/com/investree/demo/view/TransaksiService.java
@@ -6,6 +6,7 @@ import com.investree.demo.dto.TransaksiCreateRequestDTO;
 import com.investree.demo.model.Transaksi;
 
 public interface TransaksiService {
+    public Page<Transaksi> list(int page, int size,String status);
     public Map<Integer, Transaksi> save(TransaksiCreateRequestDTO dto);
     public Map<Integer, Transaksi>update(Long transaksiId);
 }
